c80c8ec9cce63b63f2787bb6533a196017ec1da6
PRANAV PARASAR <99385138+pranav9991@users.noreply.github.com>
2024-05-02 14:07:18 +0530

    Update life.py

--- life.py
+++ life.py
@@ -4,22 +4,23 @@ import pickle
 
 # Load the model
 et = pickle.load(open('life2.pkl', 'rb'))
+df = pickle.load(open('5_best.pkl','rb'))
 
 st.title('Life Expectancy Predictor')
 
 # User input fields
-infant_deaths = st.number_input('Infant deaths')
-under_five_deaths = st.number_input('Under-five deaths')
-HIV_AIDS = st.number_input('HIV/AIDS')
-Polio = st.number_input('Polio')
-BMI_random = st.number_input('BMI')
-Adult_Mortality_random = st.number_input('Adult Mortality')
-Income_composition_of_resources_random = st.number_input('Income composition of resources')
-Schooling_random = st.number_input('Schooling')
-Diphtheria_random = st.number_input('Diphtheria')
-thinness_59_years_random = st.number_input('Thinness 5-9 years')
-thinness_119_years_random = st.number_input('Thinness 1-19 years')
-Status_targuided = st.number_input('Status')
+infant_deaths = st.selectbox('Infant deaths',df['infant deaths'].unique())
+under_five_deaths = st.selectbox('Under-five deaths',df['under-five deaths '].unique())
+HIV_AIDS = st.selectbox('HIV/AIDS',df[' HIV/AIDS'].unique())
+Polio = st.selectbox('Polio',df['Polio_random'].unique())
+BMI_random = st.selectbox('BMI',df[' BMI _random'].unique())
+Adult_Mortality_random = st.selectbox('Adult Mortality',df['Adult Mortality_random'].unique())
+Income_composition_of_resources_random = st.selectbox('Income composition of resources',df['Income composition of resources_random'].unique())
+Schooling_random = st.selectbox('Schooling',df['Schooling_random'].unique())
+Diphtheria_random = st.selectbox('Diphtheria',df['Diphtheria _random'].unique())
+thinness_59_years_random = st.selectbox('Thinness 5-9 years',df[' thinness 5-9 years_random'].unique())
+thinness_119_years_random = st.selectbox('Thinness 1-19 years',df[' thinness  1-19 years_random'].unique())
+Status_targuided = st.selectbox('Status',df['Status_targuided'].unique())
 
 if st.button('Predict Life Expectancy'):
     # Gather user input into an array
